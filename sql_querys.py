0ca319c8d5daf9ec89eda72309d93f04651152ae
ksenia57 <keniabruk837@gmail.com>
2023-09-10 18:04:54 +0300

    dz

--- sql_querys.py
+++ sql_querys.py
@@ -2,6 +2,7 @@
 # Вывести топ 5 самых коротких по длительности перелетов.  Duration - разница между scheduled_arrival и scheduled_departure.
 # В ответе должно быть 2 колонки [flight_no, duration]
 TASK_1_QUERY = """
+SELECT flight_no, (scheduled_arrival-scheduled_departure) as duration FROM flights ORDER BY duration LIMIT 5
 """
 #  flight_no | duration
 # -----------+----------
@@ -17,6 +18,7 @@ TASK_1_QUERY = """
 # количество упоминаний которых меньше 50
 # В ответе должно быть 2 колонки [flight_no, count]
 TASK_2_QUERY = """
+SELECT f.* FROM (SELECT flight_no, count(*) FROM flights GROUP BY flight_no ORDER BY count DESC) as f  WHERE count<50 LIMIT 3
 """
 #  flight_no | count
 # -----------+-------
@@ -28,6 +30,13 @@ TASK_2_QUERY = """
 # Вывести число перелетов внутри одной таймзоны
 # Нужно вывести 1 значение в колонке count
 TASK_3_QUERY = """
+SELECT count(*)
+FROM flights
+LEFT JOIN airports_data as arrival
+	ON flights.arrival_airport=arrival.airport_code
+LEFT JOIN airports_data as departure
+	ON flights.departure_airport=departure.airport_code
+WHERE arrival.timezone=departure.timezone
 """
 #  count
 # --------
